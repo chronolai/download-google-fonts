a36d08811dd2f34590aa11e5a183f692ebe85982
chronolai <chronolai@synology.com>
2017-03-30 15:32:51 +0800

    refactor main.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # download-google-fonts
+
+``` shell
+$ ./main.py css Roboto # https://fonts.googleapis.com/css?family=Roboto
+$ ./main.py icon Material+Icons # https://fonts.googleapis.com/icon?family=Material+Icons
+$ ./main.py clean
+```
--- main.py
+++ main.py
@@ -6,54 +6,71 @@ import requests
 from io import BytesIO
 
 class fonts(requests.Session):
-    def __init__(self, dirname='fonts'):
+    def __init__(self, url, dir='dist'):
         super(fonts, self).__init__()
         self.headers = {
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
         }
-        self.css = '';
-        self.dirname = dirname
-        if os.path.isdir(self.dirname):
-            shutil.rmtree(self.dirname)
-        os.mkdir(self.dirname)
+        self.dir = dir
+        self.css = "%s/style.css"%dir
+        self.style = self.loadCSS(self.css)
+        self.style+= self.getCSS(url)
 
-    def loadCSS(self, url):
-        print "[Load] %s"%url
-        resp = self.get(url, headers=self.headers)
-        self.css = resp.content
-        return resp
-
-    def downloadFont(self, url):
-        resp = self.get(url);
-        with open('%s/%s'%(self.dirname, self.getFilename(url)), 'wb') as f:
-            f.write(BytesIO(resp.content).getvalue())
+        if not os.path.isdir(dir):
+            os.mkdir(dir)
 
     def getFilename(self, url):
         return url.rsplit('/', 1)[1]
 
-    def getUrls(self):
-        return re.findall('url\(([^\)]*)', self.css)
+    def getUrlsFromStyle(self, style):
+        return re.findall('url\((https[^\)]*)', style)
 
-    def saveCssFile(self):
-        urls = self.getUrls()
+    def getCSS(self, url):
+        resp = self.get(url, headers=self.headers)
+        return resp.content
+
+    def loadCSS(self, path):
+        print "Load %s"%path
+        try:
+            with open(path, 'r') as f:
+                return ''.join(f.readlines())
+        except IOError:
+            return ''
+
+    def saveCSS(self, path, style):
+        print "Save %s"%path
+        urls = self.getUrlsFromStyle(style)
         for url in urls:
             filename = self.getFilename(url)
-            self.css = self.css.replace(url, filename)
-        with open('%s/styles.css'%self.dirname, 'w') as f:
-            f.write(self.css)
+            style = style.replace(url, filename)
+        with open(path, 'w') as f:
+            f.write(style)
+
+    def downloadFile(self, url, dst):
+        resp = self.get(url);
+        with open(dst, 'wb') as f:
+            f.write(BytesIO(resp.content).getvalue())
 
 if __name__=='__main__':
-    css = fonts()
-    css.loadCSS('https://fonts.googleapis.com/css?family=Roboto')
-    for url in css.getUrls():
-        print "> %s"%url
-        css.downloadFont(url)
-    css.saveCssFile()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action')
+    parser.add_argument('-d', '--directory', default='dist')
+    parser.add_argument('family', nargs='?')
+    args = parser.parse_args()
 
-    print
-    icon = fonts('icon')
-    icon.loadCSS('https://fonts.googleapis.com/icon?family=Material+Icons')
-    for url in icon.getUrls():
-        print "> %s"%url
-        icon.downloadFont(url)
-    icon.saveCssFile()
+    if args.action in ['css', 'icon']:
+        print args.action
+        url = "https://fonts.googleapis.com/%s?family=%s"%(args.action, args.family)
+        gf = fonts(url, args.directory)
+        for url in gf.getUrlsFromStyle(gf.style):
+            dst = "%s/%s"%(gf.dir, gf.getFilename(url))
+            gf.downloadFile(url, dst)
+            print 'Download %s'%dst
+        gf.saveCSS(gf.css, gf.style)
+
+    elif args.action == 'clean':
+        print 'clean: %s'%args.directory
+        if os.path.isdir(args.directory):
+            shutil.rmtree(args.directory)
+    else:
+        parser.print_help()
